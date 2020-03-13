8b811d7c19c29bae6b86475c587a10cf46871950
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2020-03-13 20:58:24 +0300

    minus PIL

--- optical_flow.py
+++ optical_flow.py
@@ -201,8 +201,11 @@ def estimate(tensorFirst, tensorSecond):
 ##########################################################
 def convertToOpticalFlow(first_img, second_image):
     print('first image: ', first_img)
-    tensorFirst = torch.FloatTensor(numpy.array(PIL.Image.open(first_img))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
-    tensorSecond = torch.FloatTensor(numpy.array(PIL.Image.open(second_image))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+    tensorFirst = torch.FloatTensor(numpy.array(first_img)[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+    tensorSecond = torch.FloatTensor(numpy.array(second_image)[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+    # tensorFirst = torch.FloatTensor(numpy.array(PIL.Image.open(first_img))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+    # tensorSecond = torch.FloatTensor(numpy.array(PIL.Image.open(second_image))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+    print('numpy array: ', numpy.array(first_img))
 
     ttensorOutput = estimate(tensorFirst, tensorSecond)
 
