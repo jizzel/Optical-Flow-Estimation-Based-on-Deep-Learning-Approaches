53420ca5baa6d353bded86837a6d8c883c7a6949
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2020-03-14 01:10:22 +0300

    removed bash download file

--- data/downloadVideo.bash
+++
@@ -1,3 +0,0 @@
-wget -O train.mp4 https://filebin.net/r207q01l98wqat7s/train.mp4?t=ynky3xie
-wget -O test.mp4 https://filebin.net/r207q01l98wqat7s/test.mp4?t=ynky3xie
-wget https://arxiv.org/pdf/1604.07316v1.pdf
