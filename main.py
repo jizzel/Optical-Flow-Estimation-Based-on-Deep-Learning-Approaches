97de992fdd1a76337e2c71eb87a2e6b0212166ac
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2020-03-24 16:06:20 +0300

    shape

--- main.py
+++ main.py
@@ -60,12 +60,15 @@ def preprocess_data(video_input_path, flow_video_output_path, image_folder_path,
         ret, next_frame = video_reader.read()
         if next_frame is None:
             break
+        print('dimension: ', prev_frame.shape)
         print('before: ', prev_frame)
 
         prev_frame = elaborateImage(prev_frame)
         next_frame = elaborateImage(next_frame)
+        np.set_printoptions(threshold=np.nan)
 
         print('after: ', prev_frame)
+        print('dimensions: ', prev_frame.shape)
 
         bgr_flow = convertToOpticalFlow(prev_frame, next_frame)
 
