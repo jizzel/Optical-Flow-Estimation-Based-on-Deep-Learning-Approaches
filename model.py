369b0fa934d056c528a1d63283e684c7231fdbd4
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2020-04-01 01:37:13 +0300

    model modified

--- model.py
+++ model.py
@@ -1,30 +1,50 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Dropout, Reshape, BatchNormalization, ELU
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Dropout, Reshape, BatchNormalization, ELU, MaxPooling2D
 from keras.optimizers import Adam
 
 
 def CNNModel():
     model = Sequential()
-    model.add(Conv2D(24, (5, 5), input_shape=(240, 320, 3), strides=(2, 2), kernel_initializer='he_normal'))
-    model.add(ELU())
-    model.add(Conv2D(36, (5, 5), strides=(2, 2), kernel_initializer='he_normal'))
-    model.add(ELU())
-    model.add(Conv2D(48, (5, 5), strides=(2, 2), kernel_initializer='he_normal'))
-    model.add(ELU())
-    model.add(Dropout(0.5))
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), kernel_initializer='he_normal'))
-    model.add(ELU())
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='valid', kernel_initializer='he_normal'))
+    # model.add(Conv2D(24, (5, 5), input_shape=(240, 320, 3), strides=(2, 2), kernel_initializer='he_normal'))
+    # model.add(ELU())
+    # Convolution Layer 1
+    model.add(Conv2D(12, (5, 5), strides=(1, 1), activation='elu', input_shape=(240, 320, 3), kernel_initializer='he_normal'))
+    # Pooling Layer 1
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # model.add(Conv2D(36, (5, 5), strides=(2, 2), kernel_initializer='he_normal'))
+    # model.add(ELU())
+    # Convolution Layer 2
+    model.add(Conv2D(24, (5, 5), activation='elu', padding='SAME',  kernel_initializer='he_normal'))
+    # Pooling Layer 2
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # model.add(Conv2D(48, (5, 5), strides=(2, 2), kernel_initializer='he_normal'))
+    # model.add(ELU())
+    # Convolution Layer 3
+    model.add(Conv2D(36, (3, 3), activation='elu', kernel_initializer='he_normal'))
+    # Pooling Layer 3
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # model.add(Dropout(0.5))
+    # model.add(Conv2D(64, (3, 3), strides=(1, 1), kernel_initializer='he_normal'))
+    # Convolution Layer 4
+    model.add(Conv2D(48, (3, 3), activation='elu', kernel_initializer='he_normal'))
+    # Pooling Layer 4
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='valid', activation='elu', kernel_initializer='he_normal'))
+
     model.add(Flatten())
     model.add(ELU())
-    model.add(Dense(100, kernel_initializer='he_normal'))
-    model.add(ELU())
-    model.add(Dense(50, kernel_initializer='he_normal'))
+    # Dense layer 1
+    model.add(Dense(1164, activation='elu'))
+
+    model.add(Dense(100, kernel_initializer='he_normal', activation='elu'))
+    model.add(Dense(50, kernel_initializer='he_normal', activation='elu'))
     # model.add(Dropout(0.5))
-    model.add(ELU())
-    model.add(Dense(10, kernel_initializer='he_normal'))
-    model.add(ELU())
-    model.add(Dense(1, kernel_initializer='he_normal'))
+    model.add(Dense(10, kernel_initializer='he_normal', activation='elu'))
+    model.add(Dense(1, kernel_initializer='he_normal', activation='elu'))
 
     adam = Adam(lr=1e-4)
     model.compile(optimizer=adam, loss='mse', metrics=['accuracy'])
--- model3.py
+++ model3.py
@@ -9,73 +9,61 @@ def CNNModel():
     model = Sequential()
 
     # Convolution Layer 1
-    convLayer = Conv2D(filters=12,
-                       kernel_size=(5, 5),
-                       strides=(1, 1),
-                       activation='elu',
-                       input_shape=inputShape)
+    convLayer = Conv2D(filters=12, kernel_size=(5, 5), strides=(1, 1), activation='elu', input_shape=inputShape)
     model.add(convLayer)
+
     # Pooling Layer 1
-    poolingLayer = MaxPooling2D(pool_size=(2, 2),
-                                strides=(2, 2))
+    poolingLayer = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
     model.add(poolingLayer)
 
     # Convolution Layer 2
-    convLayer = Conv2D(filters=24,
-                       kernel_size=(5, 5),
-                       activation='elu')
+    convLayer = Conv2D(filters=24, kernel_size=(5, 5), activation='elu')
     model.add(convLayer)
+
     # Pooling Layer 2
-    poolingLayer = MaxPooling2D(pool_size=(2, 2),
-                                strides=(2, 2))
+    poolingLayer = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
     model.add(poolingLayer)
 
     # Convolution Layer 3
-    convLayer = Conv2D(filters=36,
-                       kernel_size=(3, 3),
-                       activation='elu')
+    convLayer = Conv2D(filters=36, kernel_size=(3, 3), activation='elu')
     model.add(convLayer)
+
     # Pooling Layer 3
-    poolingLayer = MaxPooling2D(pool_size=(2, 2),
-                                strides=(2, 2))
+    poolingLayer = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
     model.add(poolingLayer)
 
     # Convolution Layer 4
-    convLayer = Conv2D(filters=48,
-                       kernel_size=(3, 3),
-                       activation='elu')
+    convLayer = Conv2D(filters=48, kernel_size=(3, 3), activation='elu')
     model.add(convLayer)
+
     # Pooling Layer 4
-    poolingLayer = MaxPooling2D(pool_size=(2, 2),
-                                strides=(2, 2))
+    poolingLayer = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
     model.add(poolingLayer)
 
     # Flatten
     model.add(Flatten())
 
     # Dense layer 1
-    denseLayer = Dense(1164,
-                       activation='elu')
+    denseLayer = Dense(1164, activation='elu')
     model.add(denseLayer)
+
     # Dense layer 2
-    denseLayer = Dense(100,
-                       activation='elu')
+    denseLayer = Dense(100, activation='elu')
     model.add(denseLayer)
+
     # Dense layer 3
-    denseLayer = Dense(50,
-                       activation='elu')
+    denseLayer = Dense(50, activation='elu')
     model.add(denseLayer)
+
     # Dense layer 4
-    denseLayer = Dense(10,
-                       activation='elu')
+    denseLayer = Dense(10, activation='elu')
     model.add(denseLayer)
+
     # Dense layer 5
     denseLayer = Dense(1)
     model.add(denseLayer)
 
     # Compilation
-    model.compile(Adam(lr=0.001),
-                  loss='mse',
-                  metrics=['accuracy'])
+    model.compile(Adam(lr=0.001), loss='mse', metrics=['accuracy'])
 
     return model
